89a590baaaa39c67b4ea1f2df193b6b4693c706d
ThanhNha96 <134711204+ThanhNha96@users.noreply.github.com>
2025-04-10 00:53:55 +0700

    Add GameProject/Scripts/Audio/AudioManager.cs 1744221235116

--- GameProject/Scripts/Audio/AudioManager.cs
+++ GameProject/Scripts/Audio/AudioManager.cs
@@ -87,3 +87,171 @@ public class AudioManager : MonoBehaviour
     public void PlayMusic(GameState.GameScene scene)
     {
         AudioClip[] musicArray = null;
+        switch (scene)
+        {
+            case GameState.GameScene.MainMenu:
+                musicArray = mainMenuMusic;
+                break;
+            case GameState.GameScene.Gameplay:
+                musicArray = gameplayMusic;
+                break;
+            case GameState.GameScene.Shop:
+                musicArray = shopMusic;
+                break;
+        }
+
+        if (musicArray != null && musicArray.Length > 0)
+        {
+            AudioClip newTrack = musicArray[Random.Range(0, musicArray.Length)];
+            if (musicSource.clip != newTrack)
+            {
+                musicSource.clip = newTrack;
+                musicSource.Play();
+            }
+        }
+    }
+
+    public void PlayDogSound(string type)
+    {
+        AudioClip[] soundArray = null;
+        switch (type.ToLower())
+        {
+            case "bark":
+                soundArray = barkSounds;
+                break;
+            case "jump":
+                soundArray = jumpSounds;
+                break;
+            case "happy":
+                soundArray = happySounds;
+                break;
+            case "sleepy":
+                soundArray = sleepySounds;
+                break;
+        }
+
+        if (soundArray != null && soundArray.Length > 0)
+        {
+            PlayOneShot(soundArray[Random.Range(0, soundArray.Length)], "dogSound");
+        }
+    }
+
+    public void PlayVehicleSound(VehicleController.VehicleType type)
+    {
+        AudioClip[] soundArray = null;
+        switch (type)
+        {
+            case VehicleController.VehicleType.Car:
+                soundArray = carSounds;
+                break;
+            case VehicleController.VehicleType.Scooter:
+                soundArray = scooterSounds;
+                break;
+            case VehicleController.VehicleType.Bicycle:
+                soundArray = bicycleSounds;
+                break;
+            case VehicleController.VehicleType.Skateboard:
+                soundArray = skateboardSounds;
+                break;
+        }
+
+        if (soundArray != null && soundArray.Length > 0)
+        {
+            PlayOneShot(soundArray[Random.Range(0, soundArray.Length)], "vehicleSound");
+        }
+    }
+
+    public void PlayEnvironmentSound(string type)
+    {
+        AudioClip[] soundArray = null;
+        switch (type.ToLower())
+        {
+            case "birds":
+                soundArray = birdSounds;
+                break;
+            case "wind":
+                soundArray = windSounds;
+                break;
+            case "city":
+                soundArray = citySounds;
+                break;
+            case "park":
+                soundArray = parkSounds;
+                break;
+        }
+
+        if (soundArray != null && soundArray.Length > 0)
+        {
+            PlayOneShot(soundArray[Random.Range(0, soundArray.Length)], "environmentSound");
+        }
+    }
+
+    public void PlayUISound(string type)
+    {
+        AudioClip clip = null;
+        switch (type.ToLower())
+        {
+            case "click":
+                clip = buttonClick;
+                break;
+            case "collect":
+                clip = collectItem;
+                break;
+            case "complete":
+                clip = levelComplete;
+                break;
+            case "error":
+                clip = error;
+                break;
+        }
+
+        if (clip != null)
+        {
+            PlayOneShot(clip, "uiSound");
+        }
+    }
+
+    private void PlayOneShot(AudioClip clip, string sourceKey)
+    {
+        if (!soundEffects.ContainsKey(sourceKey))
+        {
+            AudioSource source = gameObject.AddComponent<AudioSource>();
+            soundEffects.Add(sourceKey, source);
+        }
+
+        AudioSource audioSource = soundEffects[sourceKey];
+        audioSource.volume = sfxVolume * masterVolume;
+        audioSource.PlayOneShot(clip);
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        masterVolume = volume;
+        PlayerPrefs.SetFloat("MasterVolume", volume);
+        UpdateVolumes();
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicVolume = volume;
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+        UpdateVolumes();
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxVolume = volume;
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+        UpdateVolumes();
+    }
+
+    private void UpdateVolumes()
+    {
+        musicSource.volume = musicVolume * masterVolume;
+        
+        foreach (var source in soundEffects.Values)
+        {
+            source.volume = sfxVolume * masterVolume;
+        }
+    }
+}
